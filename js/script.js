f32d5b8ea2de0ff81103b970eeb370931735342a
Kicadic <akshadow9@gmail.com>
2025-01-06 11:16:49 +0100

    Komentarze pomocnicze w celu lokalizacji id folderu i api

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 
-    const folderId = 'ID_FOLDERU';  // Wstaw ID folderu z Google Drive
-    const apiKey = 'TWOJ_KLUCZ_API';  // Wstaw tutaj swój klucz API Google
+    const folderId = 'ID_FOLDERU';  // Wstaw ID folderu z Google Drive. FolderID znajduje się po adresie https://drive.google.com/drive/folders/
+    const apiKey = 'TWOJ_KLUCZ_API';  // Wstaw tutaj swój klucz API Google. Znajdziesz go po ustawieniu Google Cloud.
 
     // Funkcja pobierająca listę plików z Google Drive
+  
+    // apiKey
     async function fetchImages() {
-      const url = `https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents&key=${apiKey}&fields=files(id,name,mimeType)`;
+      const url = `https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents&key=${apiKey}&fields=files(id,name,mimeType)`; 
       const response = await fetch(url);
       const data = await response.json();
 
