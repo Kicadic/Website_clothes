5b2e3c7b4b44b312f6425d8ada53a4aa3dfc324f
Kicadic <akshadow9@gmail.com>
2025-02-09 12:43:21 +0100

    Zamiana id gallery na klasę

--- js/script.js
+++ js/script.js
@@ -8,25 +8,29 @@ async function fetchImages() {
   const response = await fetch(url);
   const data = await response.json();
 
-  const gallery = document.getElementById('gallery');
+  // Pobieramy wszystkie elementy z klasą 'gallery'
+  const galleries = document.querySelectorAll('.gallery');
 
-  data.files.forEach(file => {
-    // Sprawdzamy, czy typ MIME to obraz
-    if (file.mimeType.startsWith('image/')) {
-      const imgElement = document.createElement('img');
-      // URL do wyświetlenia obrazu
-      imgElement.src = `https://drive.google.com/thumbnail?id=${file.id}&sz=w1000`;
-      imgElement.alt = file.name;  // Alternatywny tekst
-      imgElement.style.width = '200px';  // Opcjonalnie: ustawienia stylów
-      imgElement.style.margin = '10px';
-      gallery.appendChild(imgElement);
-      console.log('Generowany URL:', imgElement.src);
-    } else {
-      // Jeśli nie jest obrazem, wyświetlamy nazwę pliku jako tekst
-      const textElement = document.createElement('p');
-      textElement.textContent = `Plik: ${file.name}`;
-      gallery.appendChild(textElement);
-    }
+  galleries.forEach(gallery => {
+    data.files.forEach(file => {
+      // Sprawdzamy, czy typ MIME to obraz
+      if (file.mimeType.startsWith('image/')) {
+        const imgElement = document.createElement('img');
+        // URL do wyświetlenia obrazu
+        imgElement.src = `https://drive.google.com/thumbnail?id=${file.id}&sz=w1000`;
+        imgElement.alt = file.name;
+        imgElement.style.width = '300px';
+        imgElement.style.height = '300px';
+        imgElement.style.margin = '10px';
+        gallery.appendChild(imgElement);
+        console.log('Generowany URL:', imgElement.src);
+      } else {
+        // Jeśli nie jest obrazem, wyświetlamy nazwę pliku jako tekst
+        const textElement = document.createElement('p');
+        textElement.textContent = `Plik: ${file.name}`;
+        gallery.appendChild(textElement);
+      }
+    });
   });
 }
 
